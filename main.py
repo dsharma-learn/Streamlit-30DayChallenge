fd2076e69d195788d88dc373c1e4ee8b0c15f70d
Deepak Sharma <deepusharma@users.noreply.github.com>
2022-09-24 15:27:39 +0000

    Day 3

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import streamlit as st
 ## print("hello world")
 
-st.title("Streamlit App 2")
+#########################################
+# Day - 01
+#########################################
+st.title("Streamlit App - 30 Day Challenge")
+
+#########################################
+# Day - 02
+#########################################
+st.write('Hello world!')
+
+#########################################
+# Day - 03
+#########################################
+st.header('st.button')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
