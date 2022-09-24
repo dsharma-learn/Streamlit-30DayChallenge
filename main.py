b650b3a5239a0108ef3da7f57446956ee1cb843f
Deepak Sharma <deepusharma@users.noreply.github.com>
2022-09-24 16:03:01 +0000

    Day 06 - indentation

--- main.py
+++ main.py
@@ -93,23 +93,23 @@ def day_06():
 
     st.write('Values:', values)
 
+    # Example 3
 
-  # Example 3
-  
-  st.subheader('Range time slider')
-  
-  appointment = st.slider("Schedule your appointment:",
-                          value=(time(11, 30), time(12, 45)))
-  st.write("You're scheduled for:", appointment)
-  
-  # Example 4
-  
-  st.subheader('Datetime slider')
-  
-  start_time = st.slider("When do you start?",
-                         value=datetime(2020, 1, 1, 9, 30),
-                         format="MM/DD/YY - hh:mm")
-  st.write("Start time:", start_time)
+    st.subheader('Range time slider')
+
+    appointment = st.slider("Schedule your appointment:",
+                            value=(time(11, 30), time(12, 45)))
+    st.write("You're scheduled for:", appointment)
+
+    # Example 4
+
+    st.subheader('Datetime slider')
+
+    start_time = st.slider("When do you start?",
+                           value=datetime(2020, 1, 1, 9, 30),
+                           format="MM/DD/YY - hh:mm")
+    st.write("Start time:", start_time)
+
 
 ######################################################
 # MAIN
